6b6e5b9a96fc9000b1b2428d1417342a295ea97d
hkgsherlock <hkgsherlock@gmail.com>
2017-03-08 13:29:44 +0800

    dnlm

---
+++ src/Index/index.java
@@ -0,0 +1,15 @@
+package Index;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class index {
+	public static void main(String[] args){
+		EventQueue.invokeLater(() -> {
+	         indexFrame frame = new indexFrame();
+	         
+	      });
+	}
+	
+}
