fc018682867b3c5ce162e80354898962169c1883
hkgsherlock <hkgsherlock@gmail.com>
2017-03-13 02:31:22 +0800

    fix #4
    TODO: dynamic fetch reference if not stored reference, or return the cached reference if available.

--- src/main/java/Entity/Transactions.java
+++ src/main/java/Entity/Transactions.java
@@ -2,14 +2,14 @@ package Entity;
 
 public class Transactions {
     private final int id;
-    private final int staffId; // TODO: or store staff object reference?
-    private final int memberId; // TODO: or store member object reference?
-    private final int movieId; // TODO: or store movie object reference?
+    private final int staffId;
+    private final int customerId;
+    private final int movieId;
 
     public Transactions(int id, int staffId, int memberId, int movieId) {
         this.id = id;
         this.staffId = staffId;
-        this.memberId = memberId;
+        this.customerId = memberId;
         this.movieId = movieId;
     }
 
@@ -21,11 +21,29 @@ public class Transactions {
         return staffId;
     }
 
-    public int getMemberId() {
-        return memberId;
+    public int getCustomerId() {
+        return customerId;
     }
 
     public int getMovieId() {
         return movieId;
+    }
+
+    public Staff getStaff() {
+        // TODO: dynamic fetch reference if not stored reference,
+        // TODO: or return the cached reference if available.
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
+
+    public Customer getCustomer() {
+        // TODO: dynamic fetch reference if not stored reference,
+        // TODO: or return the cached reference if available.
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
+
+    public Movie getMovie() {
+        // TODO: dynamic fetch reference if not stored reference,
+        // TODO: or return the cached reference if available.
+        throw new UnsupportedOperationException("Not Yet Implemented");
     }
 }
